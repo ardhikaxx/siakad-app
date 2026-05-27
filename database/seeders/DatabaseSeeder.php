8a2c25e855895a5e6c2691cc2bf1a591b9accc59
ardhikaxx <ardhikayanuar58@gmail.com>
2026-05-27 09:03:24 +0700

    fix: penambahan MeetingSeeder pada database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
             RoomSeeder::class,
             ScheduleSeeder::class,
             EnrollmentSeeder::class,
+            MeetingSeeder::class,
         ]);
     }
 }
