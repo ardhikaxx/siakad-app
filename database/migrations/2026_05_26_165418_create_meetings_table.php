f8886b430f9351283f51de73795e1b36250d31ed
ardhikaxx <ardhikayanuar58@gmail.com>
2026-05-27 09:03:15 +0700

    feat: pembuatan tabel meetings untuk presensi

---
+++ database/migrations/2026_05_26_165418_create_meetings_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('meetings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('schedule_id')->constrained('schedules')->onDelete('cascade');
+            $table->string('title'); // Contoh: Pertemuan 1
+            $table->date('date');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('meetings');
+    }
+};
